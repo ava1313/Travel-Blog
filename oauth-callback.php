626dbec8555280075db29602b5885dc16f2c0b32
ava1313 <64040368+ava1313@users.noreply.github.com>
2023-09-21 15:57:17 +0300

    cms upddate

---
+++ oauth-callback.php
@@ -0,0 +1,28 @@
+<?php
+$client_id =  ebce225a13092d0c93df ;
+$client_secret = 91312e6d39176cf5067d8016b5788155e0836b11;
+
+if (isset($_GET['code'])) {
+    $code = $_GET['code'];
+    
+    // Exchange the code for an access token
+    $postdata = http_build_query([
+        'client_id' => $client_id,
+        'client_secret' => $client_secret,
+        'code' => $code,
+    ]);
+    
+    $opts = [
+        'http' => [
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata,
+        ],
+    ];
+    
+    $context  = stream_context_create($opts);
+    $response = file_get_contents('https://github.com/login/oauth/access_token', false, $context);
+    
+    // Parse the response (you'll want to store the access token in a secure way)
+    parse_str($response, $output);
+   
